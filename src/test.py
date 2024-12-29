aabd6e803bd6034059af2d9b7121ac7dc64eed83
AnavApp <anav.apparaju@gmail.com>
2024-12-28 19:39:41 -0800

    modified test.py for first backyard flight and added sitl.py

--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ import collections
 
 collections.MutableMapping = collections.abc.MutableMapping
 from dronekit import connect, VehicleMode
-from picamera2 import Picamera2, Preview
+#from picamera2 import Picamera2, Preview
 from time import sleep
 from datetime import datetime
 vehicle = None
@@ -30,12 +30,15 @@ def rtl():
 ### INITIALIZATION BLOCK ###
 # connect to drone
 connect_drone("/dev/ttyACM0")
-picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
+print("Ready to RTL")
+#picam2 = Picamera2()
+#picam2.start_preview(Preview.QTGL)
 # start recording
 
 ### INFINITE LOOP ###
-
+print("RTL in 15 seconds")
+sleep(15)
+rtl()
 #while True:
     #	flight modes: stabilize, althold, loiter, and rtl
 #	if the state is liftoff for the first time, trigger camera recording
@@ -46,10 +49,6 @@ picam2.start_preview(Preview.QTGL)
 
 #	check if there is landed property
     
-
-
-
-rtl()
 print(f"Vehicle mode is: {vehicle.mode}")
 
 ### END OF EXECUTION ###
