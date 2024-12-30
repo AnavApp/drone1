61be970acd93baa36f696743726b92b381e54a75
AnavApp <anav.apparaju@gmail.com>
2024-12-29 21:24:54 -0800

    added mac connection port

--- src/test.py
+++ src/test.py
@@ -16,6 +16,7 @@ vehicle = None
 
 def connect_drone(connection_string, waitready=True, baudrate=57600):
     global vehicle
+    print("Got here!")
     try:
         if vehicle == None:
             vehicle = connect(connection_string, wait_ready=waitready, baud=baudrate)
@@ -35,6 +36,44 @@ def connect_drone(connection_string, waitready=True, baudrate=57600):
 
     except Exception as err:
         print(Exception, err)
+
+def arm_and_takeoff(aTargetAltitude):
+    global vehicle
+    print("got to arm_and_takeoff")
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode    = VehicleMode("GUIDED")
+    vehicle.armed   = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: "), vehicle.location.global_relative_frame.alt
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            print("Reached target altitude")
+            break
+        sleep(1)
+
+
 def disconnect_drone():
     global vehicle
     vehicle.close()
@@ -48,7 +87,10 @@ def rtl():
 
 ### INITIALIZATION BLOCK ###
 # connect to drone
-connect_drone("/dev/ttyACM0")
+#connect_drone("/dev/ttyACM0") # for linux/raspberrypi
+
+connect_drone("/dev/tty.usbmodem01") # for mac
+arm_and_takeoff(20)
 print("Ready to RTL")
 #picam2 = Picamera2()
 #picam2.start_preview(Preview.QTGL)
